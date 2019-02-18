d1e9432c47e615949e2811d04fa3f34500f60d49
Charles Bett <cbrianbet@outlook.com>
2019-02-18 10:02:43 +0300

    Add and remove Personel and Cow added

--- src/com/board/Controller/AddCowServlet.java
+++ src/com/board/Controller/AddCowServlet.java
@@ -1,8 +1,8 @@
 package com.board.Controller;
 
 import java.io.IOException;
+import java.util.Date;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -11,43 +11,37 @@ import javax.servlet.http.HttpServletResponse;
 import com.board.model.*;
 
 /**
- * Servlet implementation class AddPersonelServelt
+ * Servlet implementation class AddCowServlet
  */
-@WebServlet("/AddPersonelServelt")
-public class AddPersonelServelt extends HttpServlet {
+@WebServlet("/AddCowServlet")
+public class AddCowServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	
-	Personel person;
-	PersonelList pl;
-
-    public AddPersonelServelt() {
-        
+       
+	Cow cow;
+	CowList cl;
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public AddCowServlet() {
+        super();
     }
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String name = request.getParameter("nick");
+		String tag = request.getParameter("addtagno");
+		//Date doB =  request.getParameter("bday");
 		
-		String uName = request.getParameter("addusername");
-		String empId = request.getParameter("addID");
-		String password = request.getParameter("pass");
-		String role = request.getParameter("role");
-		
-		person=new Personel(uName, empId, role, password);
-		pl = (PersonelList)request.getServletContext().getAttribute("personelList");
-		
-		pl.add(person);
-		
-		RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/managesys.jsp");
-        dispatcher.forward(request, response);
+		//cow= new Cow(name, tag, doB);
+		cl =(CowList) request.getServletContext().getAttribute("CowList");
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
 		doGet(request, response);
 	}
 
--- src/com/board/Controller/RemoveCowServlet.java
+++ src/com/board/Controller/RemoveCowServlet.java
@@ -1,53 +1,40 @@
 package com.board.Controller;
 
 import java.io.IOException;
-
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import com.board.model.*;
 
 /**
- * Servlet implementation class AddPersonelServelt
+ * Servlet implementation class RemoveCowServlet
  */
-@WebServlet("/AddPersonelServelt")
-public class AddPersonelServelt extends HttpServlet {
+@WebServlet("/RemoveCowServlet")
+public class RemoveCowServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	
-	Personel person;
-	PersonelList pl;
-
-    public AddPersonelServelt() {
-        
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public RemoveCowServlet() {
+        super();
+        // TODO Auto-generated constructor stub
     }
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		String uName = request.getParameter("addusername");
-		String empId = request.getParameter("addID");
-		String password = request.getParameter("pass");
-		String role = request.getParameter("role");
-		
-		person=new Personel(uName, empId, role, password);
-		pl = (PersonelList)request.getServletContext().getAttribute("personelList");
-		
-		pl.add(person);
-		
-		RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/managesys.jsp");
-        dispatcher.forward(request, response);
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
 
